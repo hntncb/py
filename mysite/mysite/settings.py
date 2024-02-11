2c49c84ba69dfae1f6a3ffc13b9616edb892ca42
Ha Nam <hanamtncb@gmail.com>
2024-02-11 10:24:30 +0700

    update 110224

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o8no2y9)!=zm8c=)94o0*kq3*7z3+%nm2tzxk6moz*1$$n_g0)'
+SECRET_KEY = 'django-insecure-k6ixj(u^31@jqvzzr7(k)9551urdjla9)$b!cp*3e&se5rew%3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
-from django.http import HttpResponse
-
-def index(reqest):
-    return HttpResponse("Hello")
+def index(request):
+    return HttpResponse("hello, You're at the polls index")
